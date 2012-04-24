3e6fd6ec4c87b3ab998f9754efd297db1220e953
lajos papp <lalyos@yahoo.com>
2012-04-25 01:56:04 +0200

    facebook authentication is fixed (variable token length)

--- src/com/epam/training/servlet/FacebookAuthServlet.java
+++ src/com/epam/training/servlet/FacebookAuthServlet.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -35,18 +36,25 @@ public class FacebookAuthServlet extends HttpServlet {
 	    String host = "mekbuk.epam.com";
 	    response.getWriter().println("<div>");
 	    String code = request.getParameter("code");
-	    System.out.println("### [step-1] code received from FB");
+	    System.out.println("### [step-1] code received from FB:" + code );
 		if(code != null && code.length() > 1) {
 		    String fbResp1 = getUrlResponse("https://graph.facebook.com/oauth/access_token?client_id=229865077115394&redirect_uri=http://" + host + ":8080/fbauth&client_secret=ead2f07de710df681e60428b027cc382&code=" + code);
-		    String token = fbResp1.substring(13, 123);
-		    
-		    String json = getUrlResponse("https://graph.facebook.com/me?access_token=" + token);
+		    String fbtoken = fbResp1.substring(13, fbResp1.indexOf("&expires"));
+
+		    System.out.println("### [step-2] full response:" + fbResp1);
+		    System.out.println("### [step-2] fbtoken:" + fbtoken);
+
+		    request.getSession().setAttribute("fbtoken", fbtoken);
+
+		    String json = getUrlResponse("https://graph.facebook.com/me?access_token=" + fbtoken);
 		    try {
                 JSONObject jsonObj = new JSONObject(json);
                 String username = jsonObj.getString("name");
+                String fbId = jsonObj.getString("id");
                 request.getSession().setAttribute("username", username);
+                request.getSession().setAttribute("fbid", fbId);
                 response.getWriter().println("<h1>Welcome " + username + "</h1>");
-                response.getWriter().println("<img src='https://graph.facebook.com/me/picture?access_token=" + token + "' />");
+                response.getWriter().println("<img src='http://graph.facebook.com/" + fbId + "/picture' />");
 
             } catch (JSONException e) {
                 e.printStackTrace();
